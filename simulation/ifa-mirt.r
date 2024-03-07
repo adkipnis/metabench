c28824df1c6632fe5d94d5b283c01b5ac2ed3890
adkipnis <adkipnis@mailbox.org>
2024-03-07 16:01:35 +0100

    fix parameter recovery by finding optimal column reordering

--- simulation/ifa-mirt.r
+++ simulation/ifa-mirt.r
@@ -3,15 +3,15 @@ library(dplyr)
 
 # simulation parameters
 set.seed(1)
-n <- 200 # subjects
-m <- 50 # items
-l <- 3 # latent abilities
-rho <- 0.5 # correlation between abilities 
+n <- 1000 # subjects
+m <- 25 # items
+l <- 2 # latent abilities
+rho <- 0. # correlation between abilities 
 
 # latent data
 R <- matrix(rho, l, l) + diag(l) * (1 - rho)
 theta <- MASS::mvrnorm(n, mu=rep(0,l), Sigma=R)
-A <- matrix(runif(m * l, 0.5, 1.5), m, l)
+A <- matrix(runif(m * l), m, l)
 delta <- rnorm(m)
 
 # manifest data
@@ -20,7 +20,7 @@ probabilities <- plogis(linear_term)
 p <- function(x) rbinom(1,1,x)
 responses <- data.frame(apply(probabilities, c(1,2), p))
 
-# constraints
+# starting guesses and constraints
 pars <- mirt(responses, l, itemtype = '2PL', pars = 'values')
 pars_mod <- pars %>%
   mutate(est = ifelse(name=="COV_21", T, est)) %>% # allow factor covariance
@@ -29,9 +29,17 @@ pars_mod <- pars %>%
   mutate(value = ifelse(name == "d", rnorm(m), value))
 
 # fit
-res <- mirt(responses, l, itemtype = '2PL', method = 'MHRM', pars = pars_mod)
-A_hat <- coef(res, simplify=T)$items[,1:l]
+res <- mirt(responses, l, itemtype = '2PL', method = 'MHRM',
+            pars = pars_mod,
+            )
+summary(res)
 
 # summary stats
+A_hat <- coef(res, simplify=T)$items[,1:l]
 (mse <- mean((A - A_hat)^2))
 (cor <- cor(as.vector(A), as.vector(A_hat)))
+
+# summary plots
+# plot(as.vector(A), as.vector(A_hat))
+# theta_hat <- fscores(res)
+# plot(as.vector(theta), as.vector(theta_hat))
