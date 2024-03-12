0aab646f08d737a9728b1a7e270cce68d7b55079
adkipnis <adkipnis@mailbox.org>
2024-03-12 17:57:35 +0100

    update mirt script

--- simulation/ifa-mirt.r
+++ simulation/ifa-mirt.r
@@ -5,9 +5,10 @@ source("sim-methods.r")
 library(mirt)
 
 #===============================================================================
+n <- 500
 d <- 10
-l <- 2
-data <- simulate_wide(seed=1, n=1000, d=d, l=l, rho=0.3)
+l <- 3 
+data <- simulate_wide(seed=12, n=n, d=d, l=l, rho=0.3)
 
 # starting guesses and constraints
 pars <- mirt(data$responses, l, itemtype = '2PL', pars = 'values')
@@ -18,19 +19,26 @@ pars_mod <- pars %>%
   mutate(value = ifelse(name == "d", rnorm(d), value)) # difficulty is called d in pars
 
 # fit
-res <- mirt(data$responses, l, itemtype = '2PL', method = 'MHRM',
+res <- mirt(data$responses, l, itemtype = '2PL', method = 'SEM',
             pars = pars_mod,
             )
-A_hat <- coef(res, simplify=T, rotate="none")$items[,1:l]
+estimates <-coef(res, simplify=T, rotate="none")$items
+d_hat <- estimates[,"d"]
+A_hat <- estimates[,1:l]
 A_reordered <- reorder_cols(data$A, A_hat)
 
 # summary stats
 (mse <- mean((A_reordered - A_hat)^2))
 (cor <- cor(as.vector(A_reordered), as.vector(A_hat)))
 
-# summary plots
-par(mfrow=c(1,1))
-plot(as.vector(A_reordered), as.vector(A_hat), xlim=c(0, 1.5), ylim=c(0,1.5)) 
+# vector comparisons
+par(mfrow=c(2,1))
+plot(as.vector(A_reordered), as.vector(A_hat), xlim=c(0, 10), ylim=c(0,10))
+plot(data$difficulty, -d_hat)
+
+cor(A_reordered, A_hat)
+
+# matrix comparisons
 par(mfrow=c(1,2))
 plot(A_reordered, xlab="Factor", ylab="Item", main="Original")
 plot(A_hat, xlab="Factor", ylab="Item", main="Estimate")
