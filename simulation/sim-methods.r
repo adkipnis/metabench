e515f3f15cb3a6370deabdd6f824bfb1a138126c
adkipnis <adkipnis@mailbox.org>
2024-03-12 17:56:09 +0100

    more elegant simulation pipeline

--- simulation/sim-methods.r
+++ simulation/sim-methods.r
@@ -14,19 +14,18 @@ simulate_wide <- function(seed=1, n=100, d=10, l=1, rho=0.0){
   set.seed(seed)
   
   # person parameters
-  R <- matrix(rho, l, l) + diag(l) * (1 - rho)
+  R <- matrix(rho, l, l)
+  diag(R) <- 1
   theta <- MASS::mvrnorm(n, mu=rep(0,l), Sigma=R)
   
   # item parameters
-  A <- matrix(exp(rnorm(d*l)), d, l)
+  A <- matrix(runif(d*l, min=0, max=5), d, l)
   difficulty <- rnorm(d)
-  difficulty_matrix <- t(replicate(n, difficulty))
-
+  
   # manifest data
-  linear_term <- theta %*% t(A) - difficulty_matrix
-  probabilities <- plogis(linear_term)
-  p <- function(x) rbinom(1,1,x)
-  responses <- data.frame(apply(probabilities, c(1,2), p))
+  inner = cbind(rep(1,n), theta) %*% t(cbind(difficulty, A))
+  prob = 1/(1+exp(-inner))
+  responses = data.frame(matrix(rbinom(n*d, 1, prob = prob), n, d))
   
   # output
   out <- list(theta=theta, A=A, difficulty=difficulty, responses=responses)
