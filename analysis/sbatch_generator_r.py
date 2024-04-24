c06eac5c1a02ed81b32ed0b7527154da4b620698
adkipnis <adkipnis@mailbox.org>
2024-04-24 16:03:09 +0200

    sbatch generator for r

---
+++ analysis/sbatch_generator_r.py
@@ -0,0 +1,39 @@
+import argparse
+
+def sbatchGen(benchmark: str, email: str):
+    o = f'''#!/bin/bash
+    
+#SBATCH --job-name=mb-fitting-{benchmark} 
+#SBATCH --output=logs/{benchmark}.%j.out
+#SBATCH --error=logs/{benchmark}.%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user={email}
+
+#SBATCH -p cpu_p
+#SBATCH --qos cpu_normal
+
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=24:00:00
+#SBATCH --nice=1000
+
+source $HOME/.bashrc
+Rscript fit.R {benchmark}
+    
+    '''
+    return o
+
+def main():
+    # argparse
+    parser = argparse.ArgumentParser(description='Generate sbatch script for scraping')
+    parser.add_argument('-b', '--benchmark', type=str, help='benchmark name')
+    args = parser.parse_args()
+    assert args.benchmark is not None, 'Please provide a benchmark name using the -b or --benchmark flag.'
+    
+    email = 'alexander.kipnis@helmholtz-munich.de'
+    with open(f'{args.benchmark}.sh', 'w') as f:
+        f.write(sbatchGen(args.benchmark, email))
+
+if __name__ == '__main__':
+    main()
