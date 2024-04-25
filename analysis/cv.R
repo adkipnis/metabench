2a276311a8bbe58e0be87d39042139a2250096ec
adkipnis <adkipnis@mailbox.org>
2024-04-25 16:25:42 +0200

    change to ggplot

--- analysis/cv.R
+++ analysis/cv.R
@@ -1,5 +1,5 @@
 packages <-
-  c("tidyr", "dplyr", "tibble", "readr", "mirt", "here", "caret")
+  c("tidyr", "dplyr", "tibble", "readr", "ggplot2", "mirt", "here", "caret")
 install.packages(setdiff(packages, rownames(installed.packages())))
 lapply(packages, require, character.only = T)
 
@@ -77,15 +77,16 @@ eval.model <- function(df.score, set = 'training') {
    mod.score = mgcv::gam(score ~ s(theta), data = df.score)
    df.score$p <- predict(mod.score)
    df.score <- df.score %>% arrange(by=theta)
-   plot(df.score$theta, df.score$score, pch=16, 
-      xlab=expression(theta), ylab='Score',
-      main=paste0('Score recovery (', set, ' set)'))
-   lines(df.score$theta, df.score$p, col="red")
-   train.fit <- df.score %>%
+   p <- ggplot(df.score, aes(x=theta, y=score)) +
+     geom_point() +
+     geom_line(aes(y=p), color='red') +
+     labs(x=expression(theta), y='Score') +
+     ggtitle(paste0('Score recovery (', set, ' set)'))
+   error <- df.score %>%
      summarize(mae = mean(abs(score-p)),
                sd = sd(abs(score-p))
                )
-   return(train.fit)
+   return(list(error=error, plot=p))
 }
 
 cv.fold <- function(fold, itemtype) {
@@ -98,13 +99,13 @@ cv.fold <- function(fold, itemtype) {
   theta <- get.theta(model)
   df.train <- subset.score(df.score, -fold, theta)
   cor(df.train$theta, df.train$score, method = 'spearman')
-  (train.fit <- eval.model(df.train, 'training'))
+  train.fit <- eval.model(df.train, 'training')
   
   # test performance
   theta.unseen <- get.theta(model, test)
   df.test <- subset.score(df.score, fold, theta.unseen)
   cor(df.test$theta, df.test$score, method = 'spearman')
-  (test.fit <- eval.model(df.test, 'test'))
+  test.fit <- eval.model(df.test, 'test')
   
   # summary
   out = list(model=model, train=df.train, test=df.test,
