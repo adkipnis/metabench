ecf1938434e5139befd87f0e719ec9dd179d2ac1
adkipnis <adkipnis@mailbox.org>
2024-05-07 16:38:15 +0200

    update sbatch paramrs

--- scraping/mmlu_rest.sh
+++ scraping/mmlu_rest.sh
@@ -7,12 +7,12 @@
 #SBATCH --mail-user=alexander.kipnis@helmholtz-munich.de
 
 #SBATCH -p cpu_p
-#SBATCH --qos cpu_short
+#SBATCH --qos cpu_normal
 
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=20G
-#SBATCH --time=01:00:00
+#SBATCH --mem=32G
+#SBATCH --time=06:00:00
 #SBATCH --nice=1000
 
 # activate conda env
@@ -23,6 +23,7 @@ conda activate metabench
 benchmarks="college_medicine computer_security econometrics high_school_geography high_school_microeconomics high_school_psychology high_school_statistics high_school_us_history jurisprudence miscellaneous security_studies sociology virology"
 for benchmark in $benchmarks
    do
+      echo "Processing $benchmark"
       python benchmark_loader.py -d /home/aih/alexander.kipnis/datasets/open-llm-leaderboard-cache -o /home/aih/alexander.kipnis/metabench/data -c 3 -b $benchmark
    done
 
