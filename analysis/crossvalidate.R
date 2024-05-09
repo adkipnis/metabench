0d48092718d48fc622cf3d1aaa065e641b578a53
adkipnis <adkipnis@mailbox.org>
2024-05-09 20:22:38 +0200

    adapt data loading

--- analysis/crossvalidate.R
+++ analysis/crossvalidate.R
@@ -106,43 +106,18 @@ cv.wrapper <- function(folds, itemtype, save = F) {
 
 # =============================================================================
 # prepare data
-df <- read_csv(here::here(glue("data/{BM}.csv")), show_col_types = F)
-data <- df %>%
-  mutate(correct = as.integer(correct)) %>%
-  pivot_wider(names_from = item, values_from = correct) %>%
-  column_to_rownames(var = "source")
-n_missing <- sum(is.na(data))
-rm(df)
+gprint("🚰 Loading preprocessed {BM} data...")
+preproc <- readRDS(gpath("data/{BM}_preproc.rds"))
+data <- preproc$data
+scores <- preproc$scores
 
-# remove outliers and items without variance
-scores <- rowSums(data)
-threshold <- as.numeric(quantile(scores, probs=c(0.001)))
-n <- nrow(data)
-data <- data[!(scores <= threshold),] # remove tail outliers
-std <- apply(data, 2, sd)
-m <- ncol(data)
-data <- data[, std > 0]
+# init df.score
+df.score <- data.frame(score = scores) |>
+  dplyr::mutate(rank.score = rank(score),
+                perc.score = rank.score / max(rank.score))
 
-# print summary
-summary.str <- glue(
-  "Prepared preprocessing for {BM}:\n",
-  "{n_missing} missing values (check data if > 0)\n",
-  "Removed {n - nrow(data)} tail outliers (lowest 0.1% of score, threshold: {threshold})\n",
-  "Removed {m - ncol(data)} items without variance\n",
-  "Nubmer of subjects: {nrow(data)}\n",
-  "Number of items: {ncol(data)}\n"
-  )
-print(summary.str)
-
-# df scores
-scores <- rowSums(data) # after removing outliers
-df.score <- data.frame(score = scores) %>%
-  mutate(rank.score = rank(score),
-         perc.score = rank.score / max(rank.score))
-
-# 10-fold CV split (stratified wrt. scores)
-folds <- createFolds(scores, k = 10, list = T)
-
+# init 10-fold CV split (stratified wrt. scores)
+folds <- caret::createFolds(scores, k = 10, list = T)
 
 # =============================================================================
 # fit model
