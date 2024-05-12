84cb679a2a6fb32915388d3b7a0b014d40874cb9
adkipnis <adkipnis@mailbox.org>
2024-05-12 20:20:49 +0200

    preprocess bulk

---
+++ bash/preprocess.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=mb-preprocess
+#SBATCH --output=logs/preprocess.%j.out
+#SBATCH --error=logs/preprocess.%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=alexander.kipnis@helmholtz-munich.de
+
+#SBATCH -p cpu_p
+#SBATCH --qos cpu_normal
+
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=24:00:00
+#SBATCH --nice=1000
+
+source $HOME/.bashrc
+benchmarks=($(ls ../data | grep .csv | grep -v prompts.csv | sed 's/.csv//'))
+for b in "${benchmarks[@]}"; do
+	echo "Preprocessing $b"
+	LC_ALL=C.UTF-8 Rscript analysis/preprocess.R $b
+done
