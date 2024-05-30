e85ebdc64b9e00a4a033df489118fad29e0a45e9
adkipnis <adkipnis@mailbox.org>
2024-05-30 07:16:40 +0200

    dataloaders

--- analysis/prepare.hellaswag.R
+++ analysis/prepare.hellaswag.R
@@ -9,3 +9,22 @@ box::use(./utils[mkdir, gprint, gpath, rowmerge, prop.indices, do.fa, mytheme])
 here::i_am("analysis/prepare.hellaswag.R")
 set.seed(1)
 
+# =============================================================================
+# helper functions
+collect.scores <- function(benchmark, val = F){
+   all <- readRDS(gpath("data/{benchmark}-preproc-split.rds"))
+   if (val){
+     scores <- all$scores.orig.val
+   } else {
+     scores <- all$scores.orig
+   }
+   scores.norm <- as.data.frame(100 * scores / all$max.points.orig)
+   colnames(scores.norm) <- benchmark
+   scores.norm
+}
+
+make.score.df <- function(scores, fa.res, means){
+  fs <- psych::factor.scores(scores, fa.res, method = "Bartlett")$scores
+  colnames(fs) <- paste0("F", 1:ncol(fs))
+  data.frame(fs, means)
+}
