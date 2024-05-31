298fd24f1f297b29d66872b8d17edb07f524b34c
adkipnis <adkipnis@mailbox.org>
2024-05-31 23:31:07 +0200

    new plots

--- analysis/evaluate.cv.R
+++ analysis/evaluate.cv.R
@@ -44,17 +44,78 @@ evaluate.fit <- function(df.score) {
             .groups = 'drop')
 }
 
-   box::use(ggplot2[...])
-         geom_point(alpha = 0.5) +
-         geom_abline(intercept = 0,
-                     slope = 1,
-                     linetype = "dashed") +
+plot.theta.score <- function(df.score, itemtype){
+   box::use(ggplot2[...], latex2exp[TeX])
+   df.plot <- df.score |> 
+      dplyr::filter(set == "test", type == itemtype)
+   sfs <- evaluate.fit(df.plot)
+   text <- glue::glue(
+     "RMSE = {round(sfs$rmse, 3)}\nMAE = {round(sfs$mae, 3)}\nr = {round(sfs$r, 3)}")
+   x.label <- 0.8 * diff(range(df.plot$theta)) + min(df.plot$theta)
+   y.label <- 0.1 * diff(range(df.plot$score)) + min(df.plot$score)
+   ggplot(df.plot, aes(x = theta, y = score)) +
+      geom_point(alpha = 0.5) +
+      geom_line(aes(y = p), color = "red") +
+      ylim(0,100) +
+      annotate("text", x = x.label, y = y.label, label = text, size = 3) +
+      labs(
+         title = glue::glue("Theta vs. Score ({itemtype})"),
+         x = TeX("$\\theta$"),
+         y = "Score",
+      ) +
+      mytheme()
+}
+
+plot.perc <- function(df.score, itemtype){
+   box::use(ggplot2[...], latex2exp[TeX])
+   df.plot <- df.score |>
+     dplyr::filter(set == "test", type == itemtype)
+   ggplot(df.plot, aes(x = 100 * perc.theta, y = 100 * perc.score)) +
+      geom_point(alpha = 0.5) +
+      geom_abline(intercept = 0,
+                  slope = 1,
+                  linetype = "dashed") +
+      coord_cartesian(xlim = c(0, 100), ylim = c(0, 100)) +
          labs(
+            title = glue::glue("Percentile Comparison ({itemtype})"),
+            x = TeX("$\\% \\theta$"),
+            y = "% Score",
             ) +
          mytheme()
 }
 
+
+plot.score <- function(df.score, itemtype){
+   box::use(ggplot2[...])
+   df.plot <- df.score |>
+      dplyr::filter(set == "test", type == itemtype)
+   ggplot(df.plot, aes(x = score, y = p)) +
+         geom_point(alpha = 0.5) +
+         geom_abline(intercept = 0,
+                     slope = 1,
+                     linetype = "dashed") +
+         coord_cartesian(xlim = c(0, 100), ylim = c(0, 100)) +
          labs(
+            title = glue::glue("Score Reconstruction ({itemtype})"),
+            x = "Score",
+            y = "Predicted",
+            ) +
+         mytheme()
+}
+
+plot.error <- function(df.score, itemtype){
+  box::use(ggplot2[...])
+  df.plot <- df.score |>
+    dplyr::filter(set == "test", type == itemtype) |>
+    dplyr::mutate(ae = abs(error))
+  ggplot(df.plot, aes(x = ae)) +
+   # histogram
+     geom_histogram(aes(x = ae), bins = 20, fill = "white", color = "black") +
+      coord_cartesian(xlim = c(0, 100)) +
+         labs(
+            title = glue::glue("Error Distribution ({itemtype})"),
+            x = "Absolute Error",
+            y = "Frequency"
             ) +
          mytheme()
 }
