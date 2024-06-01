75842b918471a58eaeb6eed45dad3118bdfa2e15
adkipnis <adkipnis@mailbox.org>
2024-06-01 18:21:47 +0200

    evaluate cv and fit

---
+++ bash/evaluate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=mb-evaluate
+#SBATCH --output=logs/evaluate.%j.out
+#SBATCH --error=logs/evaluate.%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=alexander.kipnis@helmholtz-munich.de
+
+#SBATCH -p cpu_p
+#SBATCH --qos cpu_normal
+
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=24:00:00
+#SBATCH --nice=1000
+
+source $HOME/.bashrc
+benchmarks=("arc" "gsm8k" "hellaswag" "truthfulqa")
+
+for b in "${benchmarks[@]}"; do
+	echo "Evaluating $b"
+	LC_ALL=C.UTF-8 Rscript ../analysis/evaluate.cv.R $b
+	LC_ALL=C.UTF-8 Rscript ../analysis/evaluate.cv.R $b EAPsum
+	LC_ALL=C.UTF-8 Rscript ../analysis/evaluate.fit.R $b
+done
+
