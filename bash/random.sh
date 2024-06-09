3a3cb04823ecd8fd85027dc402728097fabf2eea
adkipnis <adkipnis@mailbox.org>
2024-06-09 14:46:35 +0200

    server ready

--- bash/random.sh
+++ bash/random.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+#SBATCH --job-name=mb-random
+#SBATCH --output=logs/ran.%j.out
+#SBATCH --error=logs/ran.%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=alexander.kipnis@helmholtz-munich.de
+
+#SBATCH -p cpu_p
+#SBATCH --qos cpu_normal
+
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=24:00:00
+#SBATCH --nice=1000
+
+source $HOME/.bashrc
 benchmarks=("arc" "gsm8k" "hellaswag" "mmlu" "truthfulqa" "winogrande")
 sizes=(50 100 150 200 250 300 350 400 450 500)
 
