d8cfb97738e02e8176fe52ba79b37b4a9adab6ee
adkipnis <adkipnis@mailbox.org>
2024-06-11 07:44:02 +0200

    Revert "task array"
    This reverts commit 26d866231148d51fd83b35b7f82fc40f978f0ffb.

--- bash/random.sh
+++ bash/random.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=mb-random
-#SBATCH --output=logs/ran.%A_%a.out
-#SBATCH --error=logs/ran.%A_%a.err
-#SBATCH --array=1-6
+#SBATCH --output=logs/ran.%j.out
+#SBATCH --error=logs/ran.%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=alexander.kipnis@helmholtz-munich.de
 
@@ -19,11 +18,11 @@
 source $HOME/.bashrc
 benchmarks=("arc" "gsm8k" "hellaswag" "mmlu" "truthfulqa" "winogrande")
 sizes=(50 100 150 200 250 300 350 400 450 500)
-task_index=$((SLURM_ARRAY_TASK_ID-1))
-b=${models[$task_index]}
 
-for s in "${sizes[@]}"; do
-  echo "Randomly subsampling $s items from $b"
-  Rscript ../analysis/random.kfold.R $b $s
+for b in "${benchmarks[@]}"; do
+  for s in "${sizes[@]}"; do
+    echo "Randomly subsampling $s items from $b"
+    Rscript ../analysis/random.R $b $s
+  done
 done
 
