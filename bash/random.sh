26d866231148d51fd83b35b7f82fc40f978f0ffb
adkipnis <adkipnis@mailbox.org>
2024-06-11 07:35:21 +0200

    task array

--- bash/random.sh
+++ bash/random.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #SBATCH --job-name=mb-random
-#SBATCH --output=logs/ran.%j.out
-#SBATCH --error=logs/ran.%j.err
+#SBATCH --output=logs/ran.%A_%a.out
+#SBATCH --error=logs/ran.%A_%a.err
+#SBATCH --array=1-6
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=alexander.kipnis@helmholtz-munich.de
 
@@ -18,11 +19,11 @@
 source $HOME/.bashrc
 benchmarks=("arc" "gsm8k" "hellaswag" "mmlu" "truthfulqa" "winogrande")
 sizes=(50 100 150 200 250 300 350 400 450 500)
+task_index=$((SLURM_ARRAY_TASK_ID-1))
+b=${models[$task_index]}
 
-for b in "${benchmarks[@]}"; do
-  for s in "${sizes[@]}"; do
-    echo "Randomly subsampling $s items from $b"
-    Rscript ../analysis/random.R $b $s
-  done
+for s in "${sizes[@]}"; do
+  echo "Randomly subsampling $s items from $b"
+  Rscript ../analysis/random.kfold.R $b $s
 done
 
