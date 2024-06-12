d3ff9b09c3b30c617f3a32b6c6e5a140c1657830
adkipnis <adkipnis@mailbox.org>
2024-06-12 14:05:32 +0200

    rework plots and overflow
    move to logistic.R

--- analysis/logistic.R
+++ analysis/logistic.R
@@ -2,19 +2,21 @@
 
 # =============================================================================
 # custom utils, args, path, seed
-box::use(./utils[parse.args, gprint, gpath, mytheme])
+box::use(./utils[parse.args, gprint, gpath, mytheme, cbPalette])
+benchmarks <- c("arc", "gsm8k", "hellaswag", "mmlu", "truthfulqa", "winogrande")
 parse.args(
   names = c("BM" ),
-  defaults = c("winogrande"),
+  defaults = c("arc"),
   legal = list(
-    BM = c("arc", "gsm8k", "hellaswag", "mmlu", "truthfulqa", "winogrande")
+    BM = benchmarks
   )
 )
-here::i_am("analysis/logistic-recovery.R")
+here::i_am("analysis/logistic.R")
 set.seed(1)
 
 # =============================================================================
 # helper functions  
+cbp <- cbPalette()
 
 glm.train <- function(normalized.scores, data.column){
   df.log <- data.frame(y = data.column, x = normalized.scores)
@@ -34,21 +36,31 @@ get.log.likelihood <- function(thetas, params, response){
   a <- params[2]
   b <- params[1]
   inner <- thetas * a + b
-  prob <- 1 / (1 + exp(-inner))
-  response %*% t(log(prob)) + (1 - response) %*% t(log(1 - prob))
+  f <- 1 / (1 + exp(-inner))
+  
+  # Handle cases where f is close to 0 or 1
+  log_f <- ifelse(f < 1e-16, -37.6, log(f))
+  log_1_minus_f <- ifelse(f > 1 - 1e-16, -37.6, log(1 - f))
+  ll_terms <- response %*% t(log_f) + (1 - response) %*% t(log_1_minus_f)
+
+  # Apply log-sum-exp trick to reduce overflow
+  # max.term <- apply(cbind(log_f, log_1_minus_f), 1, max)
+  # ll_terms <- max.term + log(exp(ll_terms - max.term))
+  ll_terms
 }
 
 plot.score <- function(df.plot, text = ""){
+   color <- cbp[which(benchmarks == BM)]
    box::use(ggplot2[...])
    ggplot(df.plot, aes(x = score, y = p)) +
-         geom_point(alpha = 0.5) +
          geom_abline(intercept = 0,
                      slope = 1,
                      linetype = "dashed") +
+         geom_point(alpha = 0.4, color = color) +
          coord_cartesian(xlim = c(0, 100), ylim = c(0, 100)) +
-         annotate("text", x = 75, y = 25, label = text, size = 3) +
+         annotate("text", x = 75, y = 25, label = text, size = 5) +
          labs(
-            title = glue::glue("Logistic Reconstruction ({BM})"),
+            title = BM,
             x = "Score",
             y = "Predicted",
             ) +
@@ -58,11 +70,9 @@ plot.score <- function(df.plot, text = ""){
 # =============================================================================
 # prepare data
 gprint("🚰 Loading preprocessed {BM} data...")
-if (BM %in% c("hellaswag", "mmlu")){
-  datapath <- gpath("data/{BM}-sub.rds")
-} else {
-  datapath <- gpath("data/{BM}-preproc-split.rds")
-}
+datapath <- gpath("data/{BM}-preproc-split.rds")
+# datapath <- gpath("data/{BM}-sub-350.rds")
+
 preproc <- readRDS(datapath)
 data.train <- preproc$data.train
 data.test <- preproc$data.test
@@ -70,12 +80,12 @@ scores.train <- 100 * preproc$scores.train / preproc$max.points.orig
 scores.test <- 100 * preproc$scores.test / preproc$max.points.orig
 
 # estimate logistic regression models for each item
-scores.train.norm <- (scores.train - mean(scores.train))
+scores.train.norm <- scores.train
 item.params <- train.glms(scores.train.norm, data.train)
 
 # get max aposteriori estimates of scores
-prior <- density(scores.train.norm, n = 512)
-loglikelihoods <- matrix(0, nrow(data.train), 512)
+prior <- density(scores.train.norm, n = 1024, from = 0, to = 100)
+loglikelihoods <- matrix(0, nrow(data.train), 1024)
 for (i in 1:ncol(data.train)){
   ll <- function(theta) get.log.likelihood(theta, item.params[i,], data.train[,i])
   loglikelihoods <- loglikelihoods + ll(prior$x) 
@@ -83,9 +93,18 @@ for (i in 1:ncol(data.train)){
 logposterior <- loglikelihoods + log(prior$y)
 argmax <- apply(logposterior, 1, which.max)
 score.rec <- prior$x[argmax]
+if (any(is.na(logposterior))){
+  gprint("NAs produced, caution!")
+}
 
 # plot result
 rmse <- sqrt(mean((scores.train.norm - score.rec)^2))
-df.plot <- data.frame(score = scores.train, p = score.rec + mean(scores.train))
-(p <- plot.score(df.plot, glue::glue("RMSE = {round(rmse, 3)}")))
-saveRDS(p, gpath("plots/{BM}-logistic.rds"))
+gprint("Reached an RMSE of {round(rmse, 3)}.")
+df.plot <- data.frame(score = scores.train, p = score.rec)
+p <- plot.score(df.plot, glue::glue("RMSE = {round(rmse, 3)}"))
+p
+
+# save
+outpath <- gpath("plots/{BM}-logistic.rds")
+saveRDS(p, outpath)
+gprint("Saved the result to {outpath}")
