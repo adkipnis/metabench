4c048b74440da69d449ed47b38069ccc0991e523
kozzy97 <k.voudouris14@googlemail.com>
2024-09-20 10:01:16 +0200

    Add bash script for optimization run

---
+++ bash/cat-optimize.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=mb-cat-optimize
+#SBATCH --output=../../../logs/cat-optimize.out
+#SBATCH --error=../../../logs/cat-optimize.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=k.voudouris@helmholtz-munich.de
+
+#SBATCH -p cpu_p
+#SBATCH --qos cpu_normal
+
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=100G
+#SBATCH --time=10:00:00
+#SBATCH --nice=10000
+
+source $HOME/.bashrc
+cd $HOME/github-repos/metabench/analysis
+LC_ALL=C.UTF-8 Rscript cat-optimize.R
