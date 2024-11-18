95a6bb7cb26230ba94b64a6a3072768d41c64238
adkipnis <adkipnis@mailbox.org>
2024-11-18 17:15:04 +0100

    wrapper for clustering-based item selection

--- analysis/clustering.R
+++ analysis/clustering.R
@@ -70,11 +70,54 @@ sample.wrapper <- function(goal, cluster){
       indices <- c(indices, item.indices)
       cluster <- cluster |>
          dplyr::filter(!item %in% indices)
-      n <- n + k
+      n <- n + length(item.indices)
    }
    indices[1:goal]
 }
 
+make.df.score <- function(scores, theta) {
+   data.frame(score = scores, theta = theta[,1]) |>
+      dplyr::mutate(rank.score = rank(score),
+                    perc.score = rank.score / max(rank.score),
+                    rank.theta = rank(theta),
+                    perc.theta = rank.theta / max(rank.theta))
+}
+
+get.score.table <- function(theta.train, theta.test, scores.train, scores.test){
+   df.train <- make.df.score(scores.train, theta.train)
+   df.test <- make.df.score(scores.test, theta.test)
+   mod.score <- mgcv::gam(score ~ s(theta, bs = "ad"), data = df.train)
+   df.train$p <- predict(mod.score, df.train)
+   df.test$p <- predict(mod.score, df.test)
+   df.train$set <- "train"
+   df.test$set <- "test"
+   rbind(df.train, df.test) |>
+      dplyr::mutate(error = score - p)
+}
+
+run.cluster.based <- function(k, model.type, theta.type){
+   # k-means clustering
+   cluster <- get.clusters(item.params, model.type, k)
+
+   # sample items and prepare data
+   indices <- sample.wrapper(100, cluster)
+   data.train.sub <- data.train[,indices]
+   data.val.sub <- data.val[,indices]
+
+   # refit model
+   model.sub <- run.mirt(data.train.sub, 1, model.type,
+                         tol=1e-4, ncycles=1000)
+   theta.train.sub <- get.theta(model.sub, theta.type, data.train.sub)
+   theta.val.sub <- get.theta(model.sub, theta.type, data.val.sub)
+   rownames(theta.train.sub) <- rownames(data.train.sub)
+   rownames(theta.val.sub) <- rownames(data.val.sub)
+
+   # evaluate subset
+   score.table <- get.score.table(theta.train.sub, theta.val.sub,
+                                  scores.train, scores.val)
+   sfs.sub <- score.stats(score.table)
+   list(k=k, model.type=model.type, theta.type=theta.type, fits=sfs.sub)
+}
 # =============================================================================
 # prepare data
 gprint("🚰 Loading {BM} data...")
@@ -96,12 +139,4 @@ rm(full)
 # prepare item parameters
 gprint("🚰 Loading {BM} fits...")
 item.params <- napply(model.types, collect.all)
-
-# k-means clustering
-cluster <- get.clusters(item.params, "2PL", 30)
-indices <- sample.wrapper(100, cluster)
-
-# refit model
-data.sub <- data.train[indices,]
-scores.sub <- scores.train[indices]
 
