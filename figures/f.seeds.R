cd431c176a6b0f9c11fdc6ea2f98f11fd0a6e82d
adkipnis <adkipnis@mailbox.org>
2024-11-20 17:39:29 +0100

    globals

---
+++ figures/f.seeds.R
@@ -0,0 +1,11 @@
+# =============================================================================
+box::use(.. / analysis / utils[gprint, gpath, napply, mytheme, cbPalette, cbPalette2])
+box::use(.. / analysis / reduce.utils[score.stats])
+box::use(.. / analysis / reduced / best[all.benchmarks])
+here::i_am("figures/f.seeds.R")
+benchmarks <- c("arc", "gsm8k", "hellaswag", "mmlu", "truthfulqa", "winogrande", "metabench")
+names <- c("ARC", "GSM8K", "Hellaswag", "MMLU", "TruthfulQA", "WinoGrande", "metabench")
+point.size <- 5
+mean.size <- 5
+mean.shape <- 4
+
